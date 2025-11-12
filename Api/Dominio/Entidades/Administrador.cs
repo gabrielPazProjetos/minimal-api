70de08700879761a834f888df2215409643d46ab
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-11-12 12:19:33 -0300

    Update Administrador.cs

--- Api/Dominio/Entidades/Administrador.cs
+++ Api/Dominio/Entidades/Administrador.cs
@@ -7,17 +7,17 @@ public class Administrador
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id { get;set; } = default!;
+    public int Id { get; set; }
 
     [Required]
     [StringLength(255)]
-    public string Email { get;set; } = default!;
+    public string Email { get; set; } = string.Empty;
 
     [Required]
     [StringLength(50)]
-    public string Senha { get;set; } = default!;
+    public string Senha { get; set; } = string.Empty;
 
     [Required]
     [StringLength(10)]
-    public string Perfil { get;set; } = default!;
+    public string Perfil { get; set; } = string.Empty;
 }
